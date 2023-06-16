ceb05c8da4e2f57e42ab274485bba2a064568005
Shreya Porwal <111834212+porwalshreyaa@users.noreply.github.com>
2023-06-16 22:01:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const searchInput = document.querySelector("#search-input");
 
 searchInput.addEventListener("keydown", function(event) {
-    if(event.code === "Enter") {
+    if(event.keyCode === 13) {
         search();
     }
 });

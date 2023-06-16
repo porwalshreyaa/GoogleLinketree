b4204c81c84565b4e43e7ad6cc24dc688acc001d
Shreya Porwal <111834212+porwalshreyaa@users.noreply.github.com>
2023-06-16 15:46:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("keydown", function(event) {
+    if(event.code === "Enter") {
+        search();
+    }
+});
+
+function search() {
+    const input = searchInput.value;
+
+    window.location.href = "https://www.google.com/search?q="+ input +"&rlz=1C1CHBD_enIN926IN927&oq=" +input+"&aqs=chrome.0.0i355i433i512j46i340i433i512l2j69i57j46i433i512j0i512j69i60l2.5413j0j7&sourceid=chrome&ie=UTF-8"
+}
